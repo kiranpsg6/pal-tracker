a0f85512429d3fe300d2dd82e18b3ff85b82be13
kiranpsg6 <kiran.psg6@gmail.com>
2019-09-18 00:33:03 -0700

    fix crud operation issue

--- src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -1,71 +1,58 @@
 package io.pivotal.pal.tracker;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class InMemoryTimeEntryRepository implements  TimeEntryRepository {
+public class InMemoryTimeEntryRepository implements TimeEntryRepository {
+    private HashMap<Long, TimeEntry> timeEntries = new HashMap<>();
 
-    List<TimeEntry> timeEntryList = new ArrayList<>();
+    private long currentId = 1L;
 
-    long timeEntryId = 0L;
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+        Long id = currentId++;
 
-    public TimeEntry create(TimeEntry timeEntry){
+        TimeEntry newTimeEntry = new TimeEntry(
+                id,
+                timeEntry.getProjectId(),
+                timeEntry.getUserId(),
+                timeEntry.getDate(),
+                timeEntry.getHours()
+        );
 
-        timeEntryId = timeEntryId + 1L;
-
-        timeEntry.setId(timeEntryId);
-
-        timeEntryList.add(timeEntry);
-        return timeEntry;
+        timeEntries.put(id, newTimeEntry);
+        return newTimeEntry;
     }
 
-    public TimeEntry find(Long timeEntryId){
-
-        for(TimeEntry timeEntry: timeEntryList){
-            if(timeEntry.getId() == timeEntryId ){
-                return  timeEntry;
-            }
-        }
-        return null;
+    @Override
+    public TimeEntry find(Long id) {
+        return timeEntries.get(id);
     }
 
-    public List<TimeEntry> list(){
-        return timeEntryList;
+    @Override
+    public List<TimeEntry> list() {
+        return new ArrayList<>(timeEntries.values());
     }
 
-    public TimeEntry update (Long timeEntryId, TimeEntry timeEntry){
+    @Override
+    public TimeEntry update(Long id, TimeEntry timeEntry) {
+        if (find(id) == null) return null;
 
-        int foundIndex = -1;
+        TimeEntry updatedEntry = new TimeEntry(
+                id,
+                timeEntry.getProjectId(),
+                timeEntry.getUserId(),
+                timeEntry.getDate(),
+                timeEntry.getHours()
+        );
 
-        for(int i =0 ; i< timeEntryList.size() ; i++){
-            if(timeEntryList.get(i).getId() == timeEntryId){
-                    foundIndex = i;
-                    break;
-            }
-        }
-
-        if(foundIndex != -1){
-            timeEntry.setId(timeEntryId);
-            timeEntryList.add(foundIndex, timeEntry);
-            return timeEntry;
-        }
-        else{
-            return  null;
-        }
+        timeEntries.replace(id, updatedEntry);
+        return updatedEntry;
     }
 
-    public void delete(Long timeEntryId){
-
-            int foundIndex = -1;
-
-            for (int i = 0; i < timeEntryList.size(); i++) {
-                if (timeEntryList.get(i).getId() == timeEntryId) {
-                    foundIndex = i;
-                    break;
-                }
-            }
-            if (foundIndex != -1) {
-                timeEntryList.remove(foundIndex);
-            }
-        }
+    @Override
+    public void delete(Long id) {
+        timeEntries.remove(id);
+    }
 }
--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -3,9 +3,6 @@ package io.pivotal.pal.tracker;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
-import java.sql.Time;
 import java.util.List;
 
 @RestController
